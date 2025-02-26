7f800291d04ae141783caf373f90a9bc53c578b8
zacharydesoto <zacharysdesoto@gmail.com>
2025-02-25 16:59:38 -0800

    Work on Movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from enum import Enum
+from collections import deque
 
 from utils import *
 from movement import *
@@ -7,24 +8,18 @@ from movement import *
 pygame.init()
 
 config = {}
-SCREEN_WIDTH, SCREEN_HEIGHT  = 800, 800
+SCREEN_WIDTH, SCREEN_HEIGHT  = 800, 800  # Make sure screen width and height are multiples of grid width and height
 GRID_WIDTH, GRID_HEIGHT = 10, 10
+MARGIN = 10
+SQUARE_WIDTH, SQUARE_HEIGHT = SCREEN_WIDTH/GRID_WIDTH, SCREEN_HEIGHT/GRID_HEIGHT
 config['SCREEN_WIDTH'], config['SCREEN_HEIGHT'] = SCREEN_WIDTH, SCREEN_HEIGHT
 config['GRID_WIDTH'], config['GRID_HEIGHT'] = GRID_WIDTH, GRID_HEIGHT
+config['MARGIN'] = MARGIN
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH+2*MARGIN, SCREEN_HEIGHT+2*MARGIN))
 clock = pygame.time.Clock()
 
 pygame.display.set_caption('SnakeRL')
-
-
-SQUARE_WIDTH, SQUARE_HEIGHT = SCREEN_WIDTH/GRID_WIDTH, SCREEN_HEIGHT/GRID_HEIGHT
-
-class Square(Enum):
-    EMPTY = 0
-    PLAYER1 = 1
-    PLAYER2 = 2
-    TOMATO = 3
 
 grid = [[Square.EMPTY] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
 grid[4][2] = Square.PLAYER1
@@ -32,11 +27,26 @@ grid[4][1] = Square.PLAYER1
 grid[4][6] = Square.PLAYER2
 grid[4][7] = Square.PLAYER2
 grid[4][4] = Square.TOMATO
+grid[0][1] = Square.TOMATO
+
+snake1, snake2 = deque(), deque()
+snake1.append((4, 2))
+snake1.append((4, 1))
+snake2.append((4, 6))
+snake2.append((4, 7))
+head1_dir = Direction.RIGHT
+head2_dir = Direction.LEFT
+
+game_state = (grid, snake1, head1_dir, snake2, head2_dir, False)
 
 run = True
 while run:
     key = pygame.key.get_pressed()
-    grid = handle_movement(grid, key)
+
+    game_state = handle_movement(game_state, key, config)
+    grid, snake1, head1_dir, snake2, head2_dir, game_end = game_state
+    if game_end:
+        run = False
 
     screen.fill((0, 0, 0))
 
@@ -57,6 +67,6 @@ while run:
     
     pygame.display.update()
 
-    clock.tick(60)
+    clock.tick(1)
 
 pygame.quit()
--- movement.py
+++ movement.py
@@ -1,6 +1,72 @@
 import pygame
+from enum import Enum
 
-def handle_movement(grid, key):
-    # if key[pygame.K_a]:
+class Direction(Enum):
+    LEFT = (0, -1)
+    UP = (-1, 0)
+    RIGHT = (0, 1)
+    DOWN = (1, 0)
 
-    return grid
+class Square(Enum):
+    EMPTY = 0
+    PLAYER1 = 1
+    PLAYER2 = 2
+    TOMATO = 3
+
+def handle_movement(game_state, key, config):
+    grid, snake1, head1_dir, snake2, head2_dir, game_end = game_state
+
+    if key[pygame.K_a]:
+        input1 = Direction.LEFT
+    elif key[pygame.K_s]:
+        input1 = Direction.DOWN
+    elif key[pygame.K_d]:
+        input1 = Direction.RIGHT
+    elif key[pygame.K_w]:
+        input1 = Direction.UP
+    else:
+        input1 = head1_dir
+    
+    if key[pygame.K_UP]:
+        input2 = Direction.UP
+    elif key[pygame.K_DOWN]:
+        input2 = Direction.DOWN
+    elif key[pygame.K_RIGHT]:
+        input2 = Direction.RIGHT
+    elif key[pygame.K_LEFT]:
+        input2 = Direction.LEFT
+    else:
+        input2 = head2_dir
+
+    def check_perpendicular_directions(dir1, dir2):
+        return (dir1 in [Direction.LEFT, Direction.RIGHT]) != (dir2 in [Direction.LEFT, Direction.RIGHT])
+    
+    if check_perpendicular_directions(head1_dir, input1):
+        head1_dir = input1
+    if check_perpendicular_directions(head2_dir, input2):
+        head2_dir = input2
+
+    head1_pos = snake1[0]
+    head2_pos = snake2[0]
+    new_head1_pos = head1_pos + head1_dir.value
+    new_head2_pos = head2_pos + head2_dir.value
+    tail1_pos = snake1.popleft()
+    tail2_pos = snake2.popleft()
+
+    if new_head1_pos[0] < 0 or new_head1_pos[0] > config['GRID_HEIGHT'] or new_head1_pos[1] < 0 or new_head1_pos[1] > config['GRID_WIDTH']:
+        game_end = True
+    if new_head2_pos[0] < 0 or new_head2_pos[0] > config['GRID_HEIGHT'] or new_head2_pos[1] < 0 or new_head2_pos[1] > config['GRID_WIDTH']:
+        game_end = True
+
+    print(new_head1_pos)
+    print(new_head2_pos)
+    grid[new_head1_pos[0]][new_head1_pos[1]] = Square.PLAYER1
+    grid[new_head2_pos[0]][new_head2_pos[1]] = Square.PLAYER2
+    snake1.append(new_head1_pos)
+    snake2.append(new_head2_pos)
+
+    # print(head1_pos)
+    # print(head2_pos)
+    
+    game_state = (grid, snake1, head1_dir, snake2, head2_dir, game_end)
+    return game_state
